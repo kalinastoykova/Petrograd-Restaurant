667c798845b44c88fdc88c41cdeb0ac8c7c67ba3
kalinastoykova <kali0173@stud.kea.dk>
2020-09-06 23:06:41 +0200

    most is done :)))

--- script.js
+++ script.js
@@ -1,37 +1,125 @@
-// fetch the data
-fetch("https://kea-alt-del.dk/t5/api/productlist")
-    .then(e => e.json())
-    .then(dataReceived)
+//get categories
+function init() {
+    fetch("https://kea-alt-del.dk/t5/api/categories").then(r => r.json()).then(
+        function (data) {
+            categoriesRec(data)
+        }
+    );
+}
+init();
 
-function dataReceived(foodItems) {
-    //loop through the food items
-    foodItems.forEach(showItem)
+function categoriesRec(categ) {
+    createNav(categ);
+    createSec(categ);
+    getProducts();
 }
 
-//executed once for each product
-function showItem(myItem) {
-    console.log(myItem);
-    //find the template
-    const temp = document.querySelector("#productTemplate").content;
+function createSec(categories) {
+    categories.forEach(category => {
+        const section = document.createElement("section");
+        section.setAttribute("id", category);
+        const h3 = document.createElement("h3");
+        h3.textContent = category;
+        section.appendChild(h3);
+        document.querySelector(".productList").appendChild(section);
+    });
+}
 
-    //image variables
-    const imageName = myItem.image;
+function createNav(categories) {
+    categories.forEach(categ => {
+        console.log(categ);
+        const a = document.createElement("a");
+        a.textContent = categ;
+        a.setAttribute("href", `#${categ}`);
+        document.querySelector("#menuList").appendChild(a);
+    });
+}
+
+
+function getProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (resp) {
+            console.log(resp);
+            return resp.json();
+        })
+        .then(function (data) {
+            dataRec(data);
+        })
+}
+
+function dataRec(products) {
+    products.forEach(showProd);
+}
+
+function showProd(myProd) {
+    console.log(myProd);
+
+    const temp = document.querySelector("#productTemplate").content;
+    const myCopy = temp.cloneNode(true);
+
+    const imageName = myProd.image;
     const base = "https://kea-alt-del.dk/t5/site/imgs/";
     const smallImg = base + "small/" + imageName + "-sm.jpg";
     const mediumImg = base + "medium/" + imageName + "-md.jpg";
     const largeImg = base + "large/" + imageName + ".jpg";
 
-    //clone the template
-    const myCopy = temp.cloneNode(true);
+    myCopy.querySelector(".productName").textContent = myProd.name;
+    myCopy.querySelector(".shortDesc").textContent = myProd.shortdescription;
+    myCopy.querySelector(".price").textContent = myProd.price + "-,";
+    myCopy.querySelector(".imgFood").src = mediumImg;
 
-    //fill the template with content
-    myCopy.querySelector(".productName").textContent = myItem.name;
-    myCopy.querySelector(".shortDesc").textContent = myItem.shortdescription;
-    myCopy.querySelector(".price").textContent = myItem.price + "-,";
-	myCopy.querySelector(".imgFood").src = largeImg;
+    if (!myProd.discount) {
+        console.log("NO DISCOUNT");
+        myCopy.querySelector(".sale").classList.add("hidden")
+    }
+    if (myProd.discount) {
+        myCopy.querySelector(".price").classList.add("strike")
+        myCopy.querySelector(".sale").textContent = myProd.price - myProd.discount + "-,";
+    }
+    if (!myProd.vegetarian) {
+        console.log("NOT VEGETARIAN");
+        myCopy.querySelector(".vegetarian").classList.add("hidden")
+    }
+    if (myProd.soldout) {
+        console.log("SOLD OUT")
+        const p = document.createElement("p");
+        myCopy.querySelector(".imgFood").style.opacity = "0.5"
+        p.textContent = "SOLD OUT";
+        p.classList.add("soldOutP");
+        myCopy.querySelector("article").appendChild(p);
+    }
+    if (!myProd.alcohol) {
+        console.log("NON-ALCOHOLIC");
+        myCopy.querySelector(".alcoholic").classList.add("hidden");
+    }
 
+    myCopy.querySelector(".moreInfo").addEventListener("click", () => {
+        fetch(`https://kea-alt-del.dk/t5/api/product?id=` + myProd.id)
+            .then(res => res.json())
+            .then(readMore);
+    });
 
-    //apend
-    const parentElem = document.querySelector("section#starters");
+    const parentElem = document.querySelector("section#" + myProd.category);
     parentElem.appendChild(myCopy);
+
+    const modal = document.querySelector(".modalBackground");
+
+    function readMore(data) {
+        console.log(data);
+        modal.querySelector(".modalName").textContent = data.name;
+        modal.querySelector(".modalDescr").textContent = data.longdescription;
+        modal.style.display = "block";
+        modal.querySelector(".modalPrice").textContent = myProd.price + "-,";
+        modal.querySelector(".modalImg").src = mediumImg;
+    }
+
+    modal.addEventListener("click", function () {
+        this.style.display = "none"
+    });
+}
+
+const modal = document.querySelector(".modalBackground")
+function showDet(data) {
+    console.log(data);
+    modal.querySelector(".modalName").textContent = data.name;
 }
